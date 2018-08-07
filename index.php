5f340017765c81a3f7b1995127ef08815bc6c070
tlacolley <thomaslacolley@gmail.com>
2018-08-07 09:47:43 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Projet CRM</title>
+    </head>
+    <body>
+        <h1>Hello</h1>
+    </body>
+</html>
